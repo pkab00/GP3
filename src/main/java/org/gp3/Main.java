3dd62e5f332f8ab72bf94bf05b3974e85bcc8d8b
vbshkn <pika_pikabu@vk.com>
2025-04-02 12:03:12 +0700

    Fixed dependencies (there's still a little error message)

--- src/main/java/org/gp3/Main.java
+++ src/main/java/org/gp3/Main.java
@@ -7,6 +7,7 @@ public class Main {
         String fullPath = "D:\\GP3\\src\\test\\test_audio\\(G)I-DLE - Klaxon.mp3";
         Song klaxon = new Song(fullPath);
         System.out.println(klaxon);
+
         ArrayList<Playable> songs = new ArrayList<>();
         songs.add(klaxon);
     }
