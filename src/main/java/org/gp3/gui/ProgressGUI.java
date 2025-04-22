db1c6b5d42bad514cca81ea8ff4b78282b06aab2
vbshkn <pika_pikabu@vk.com>
2025-04-22 17:29:12 +0700

    V. 1.2.0

--- src/main/java/org/gp3/gui/ProgressGUI.java
+++ src/main/java/org/gp3/gui/ProgressGUI.java
@@ -2,6 +2,9 @@ package org.gp3.gui;
 
 import javax.swing.*;
 
+/**
+ * Мини-окно для отображения прогресса добавления песен в плейлист.
+ */
 public class ProgressGUI extends JFrame {
     private JProgressBar bar;
     private JPanel rootPane;
@@ -16,15 +19,26 @@ public class ProgressGUI extends JFrame {
         setVisible(true);
     }
 
+    /**
+     * Устанавливает максимальное значение прогресса
+     * @param maximum максимальное значение
+     */
     public void setMaximum(int maximum){
         bar.setMaximum(maximum);
     }
 
+    /**
+     * Обновляет прогресс добавления песен.
+     * @param progress текущее значение
+     */
     public void updateProgress(int progress){
         bar.setValue(progress);
         label.setText(String.format("Добавлено песен: %d/%d", progress, bar.getMaximum()));
     }
 
+    /**
+     * Закрывает окно по завершении процесса.
+     */
     public void done(){
         dispose();
     }
